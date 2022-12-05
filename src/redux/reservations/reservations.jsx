1e7811477072a42f15d7309a3d2b0c551231192c
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-05 03:58:38 -0500

    Add reducer for reservation form

--- src/redux/reservations/reservations.jsx
+++ src/redux/reservations/reservations.jsx
@@ -22,5 +22,20 @@ export const addReservation = (data) => async (dispatch) => {
 };
 
 
+//initialize the state 
+const initialState = {
+    reservations: [],
+};
 
-
+// reducers
+export const reservationsReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case ADD_RESERVATION:
+        return {
+            ...state,
+            reservations: [...state.reservations, action.payload],
+        };
+      default:
+        return state;
+    }
+};
