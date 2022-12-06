8b815938d4b4a62e8cc012671c1f32f5c9cec394
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-06 17:09:36 -0500

    Add form for reservations

--- src/component/reservations/reservationsform.jsx
+++ src/component/reservations/reservationsform.jsx
@@ -2,3 +2,7 @@ import React from 'react';
 import { CFormSelect, CButton, CCol } from '@coreui/react';
 import uuid from 'react-uuid';
 import dummy from '../../dummyApi/dummy';
+
+const Form = () => (
+    <>
+      <CFormSelect size="lg" className="mb-3" aria-label="Large select example">
