7117b0e20bfd7f479b494ff2fbe111381f5244f3
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-07 08:11:37 -0500

    Add form route for reservation

--- src/pages/reservation.jsx
+++ src/pages/reservation.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable import/extensions */
 import React from 'react';
+import Form from '../component/reservations/reservationsform';
 
 function Reservation() {
   return (
     <div>
       <h1>Reservation page</h1>
-
+      <Form />
     </div>
   );
 }
