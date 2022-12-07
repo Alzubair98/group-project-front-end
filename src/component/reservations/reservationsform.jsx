844fbd4d0796533bb7fed5b893407325124cf34f
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-07 08:10:20 -0500

    Add form option user for reservation

--- src/component/reservations/reservationsform.jsx
+++ src/component/reservations/reservationsform.jsx
@@ -18,6 +18,20 @@ const Form = () => ( <>
     }
     </CFormSelect>
     <CFormSelect size="sm" className="mb-3" aria-label="Small select example">
-      
+    <option>Select a User/Owner</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </CFormSelect>
+    <CCol xs={12}>
+      <CButton color="primary" type="submit">
+        Submit form
+      </CButton>
+    </CCol>
+
+  </>
+);
+
+export default Form;
 
    
