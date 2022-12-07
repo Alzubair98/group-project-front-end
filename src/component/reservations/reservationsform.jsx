6b1d8eec62a1461e99838d99ce00ec173cdfe57f
ijeomaodinko <ijeomaodinko@gmail.com>
2022-12-07 08:08:42 -0500

    Add form option for reservation

--- src/component/reservations/reservationsform.jsx
+++ src/component/reservations/reservationsform.jsx
@@ -3,5 +3,19 @@ import { CFormSelect, CButton, CCol } from '@coreui/react';
 import uuid from 'react-uuid';
 import dummy from '../../dummyApi/dummy';
 
-const Form = () => (
+const Form = () => ( <>
+    <CFormSelect size="lg" className="mb-3" aria-label="Large select example">
+      <option>Select and reserve a bike</option>
+      {
+      dummy.map((bike, index) => (
+        <option value={index} key={uuid()}>
+          react-elastic-carousel@`&quot;`^0.11.5`&quot;`
+
+          {bike.name}
+
+        </option>
+      ))
+    }
+    
+
    
