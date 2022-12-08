11d3a13242148ce7158fb1914b188e11ee9ec06d
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-12-08 19:18:39 +0300

    add home.js

---
+++ src/component/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+import Login from "./auth/Login";
+import Registration from "./auth/Registration";
+import "./home.css";
+
+function Home(props) {
+  const handleLogoutClick = () => {
+    axios
+      .delete("http://localhost:3001/logout", { withCredentials: true })
+      .then((response) => {
+        props.handleLogout();
+      })
+      .catch((error) => {
+        console.log("logout error", error);
+      });
+    props.handleLogout();
+  };
+
+  return (
+    <div>
+      <div className="home-div">
+        <Login />
+        <Registration />
+      </div>
+      <button onClick={() => handleLogoutClick()}>logout</button>
+    </div>
+  );
+}
+
+export default Home;
