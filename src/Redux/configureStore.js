b66d41adb5115d28676d815be057be568728b637
Aneal Laryea <aneallaryea100@gmail.com>
2022-12-12 21:23:02 +0000

    Update bikes component

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import bikeSlice from './bike/bike';
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    bike: bikeSlice,
   },
 });
 
